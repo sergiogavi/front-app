a539224ee9e5313e83d3a5d43260fe7b1c24ea07
sergiogavi <sergio.gavi9@gmail.com>
2023-04-03 19:31:09 +0200

    Recuperando información de los usuarios
    Maquetación

--- src/components/Comunicate.js
+++ src/components/Comunicate.js
@@ -2,16 +2,37 @@ import React, { useState, useEffect } from "react";
 
 
 export default function Comunicate() {
+  let nombreDef;
   fetch("http://localhost:8080/api/prueba")
   .then(response => response.text())
   .then((response) => {
-      console.log(response)
-      document.getElementById("demo").innerHTML = response;
-  })
+    var myArr = JSON.parse(response);
+      console.log(response);
+   /*   myArr.forEach(element => {
+        nombreDef=document.getElementById("demo").innerHTML = element.nombre; 
+        console.log(element.nombre);
+        document.getElementById("demo").innerHTML = element.nombre; 
+      });*/
+       
+      var tbody = document.getElementById('demo');
+      tbody.innerHTML = "";
+
+    for (var i = 0; i < myArr.length; i++) {
+      var tr = "<tr>";
+      tr += "<td>ID</td>" + "<td>" + myArr[i].id + "</td></tr>";
+      tr += "<td>Nombre : </td>" + "<td>" + myArr[i].nombre + "</td></tr>";
+      tr += "<td>apellido : </td>" + "<td>" + myArr[i].apellido + "</td></tr>";
+      tr += "<td>telefono : </td>" + "<td>" + myArr[i].telefono + "</td></tr>";
+      tr += "<td>email : </td>" + "<td>" + myArr[i].email + "</td></tr>";
+      tr += "<hr>";
+      tbody.innerHTML += tr;
+}})
   .catch(err => console.log(err))
   return (
     <div className="Comunicate">
-     <p id="demo"></p>
+ <table>
+  <tbody id="demo"></tbody>
+</table>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,8 @@
-import React, { useState, useEffect } from "react";
-
-
 export default function Comunicate() {
-  const [message, setMessage] = useState("");
-  let myJSON;
-  useEffect(() => {
-    fetch("http://localhost:8080/api/prueba")
-      .then((res) => res)
-      .then((data) => setMessage(data.message));
-  }, []);
-  //let v=document.getElementById("demo").innerHTML = JSON.stringify(message);
-  console.log("El valor de message es: "+message);
+ 
   return (
     <div className="Comunicate">
-     <p id="demo">{message}</p>
+     <h1>Home Page</h1>
     </div>
   );
 }
